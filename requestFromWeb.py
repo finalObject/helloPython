0ca6290c4737450eb7cb30d80e477ac6ad9329d9
X. HU(Mac) <i@finalobject.cn>
2017-10-24 12:06:49 +0800

    add web crawler

---
+++ requestFromWeb.py
@@ -0,0 +1,20 @@
+#python3!
+#抓去网页图片
+#执行之后都存在根目录，自己注意
+import urllib.request as myUrl
+import re
+
+req = myUrl.urlopen('http://www.imooc.com/course/list')
+buf = req.read()
+
+buf = buf.decode('utf-8')
+#写入文档的时候也需要先decode，不能强制转换
+#buf = str(buf) 不知道为啥，这个不能用
+
+list1 = re.findall(r'//img.*?jpg',buf)
+
+i=0
+for url in list1:
+	url = 'http:'+url
+	myUrl.urlretrieve(url,str(i)+'.jpg')
+	i = i+1
--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-class FileError(IOError):
-    def __init__(self,info):
-        Exception.__init__(self)
-        self.errorInfo = info
-    def __str__(self):
-        return "FileError:%s" % self.errorInfo
-try:
-    raise FileError("FF")
-except FileError as e:
-    print(e) 
+import re
+str1 = 'imooc python'
+pa = re.compile('imooc')
